63fc0f5ff3b9414128fa7f4ae8aead0be541a0a6
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-12-24 12:07:42 +0900

    ✨ : reactive-function

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, reactive } from 'vue'
 
-// 半径の初期値を乱数で取得
-const radiusInit = Math.round(Math.random() * 10)
-// 円周率のテンプレート変数を用意
-const PI = ref(3.14)
-// 半径のテンプレート変数を用意
-const radius = ref(radiusInit)
+// リアクティブなテンプレート変数をまとめて用意
+const data = reactive({
+  PI: 3.14,
+  radius: Math.round(Math.random() * 10)
+})
 // 円の面積の算出プロパティを用意
-const area = computed((): number => radius.value * radius.value * PI.value)
+const area = computed(() => data.radius * data.radius * data.PI)
 // 半径のテンプレート変数に新しい乱数を1秒ごとに格納
-setInterval((): void => {
-  radius.value = Math.round(Math.random() * 10)
+setInterval(() => {
+  data.radius = Math.round(Math.random() * 10)
 }, 1000)
 </script>
 
 <template>
-  <p>半径{{ radius }}の円の面積を円周率{{ PI }}で計算すると{{ area }}</p>
+  <p>半径{{ data.radius }}の円の面積を円周率{{ data.PI }}で計算すると{{ area }}</p>
 </template>
