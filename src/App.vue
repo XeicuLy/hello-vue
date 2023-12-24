c987395eb7adfa052f88d0c44b7cd82a4ab867b8
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-12-24 12:02:11 +0900

    ✨ : computed

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
-// 現在日時を取得
-const now = new Date()
-// 現在の時刻の文字列を取得
-const nowStr = now.toLocaleTimeString()
-// 現在の時刻文字列をテンプレート変数として用意
-let timeStr = nowStr
-// 同じく現在の時刻文字列をテンプレート変数としてref()で用意
-const timeStrRef = ref(nowStr)
-// 新しい時刻に変更する関数
-const changeTime = () => {
-  // 現在日時を取得
-  const newTime = new Date()
-  //  現在の時刻の文字列を取得
-  const newTimeStr = newTime.toLocaleTimeString()
-  // 現在の時刻文字列をテンプレート変数timeStrに格納
-  timeStr = newTimeStr
-  // 現在の時刻文字列をテンプレート変数timeStrRefに格納
-  timeStrRef.value = newTimeStr
-}
-// changeTime関数を1秒ごとに実行
-setInterval(changeTime, 1000)
+// 半径の初期値を乱数で取得
+const radiusInit = Math.round(Math.random() * 10)
+// 円周率のテンプレート変数を用意
+const PI = ref(3.14)
+// 半径のテンプレート変数を用意
+const radius = ref(radiusInit)
+// 円の面積の算出プロパティを用意
+const area = computed((): number => radius.value * radius.value * PI.value)
+// 半径のテンプレート変数に新しい乱数を1秒ごとに格納
+setInterval((): void => {
+  radius.value = Math.round(Math.random() * 10)
+}, 1000)
 </script>
 
 <template>
-  <p>現在時刻: {{ timeStr }}</p>
-  <p>現在時刻(ref): {{ timeStrRef }}</p>
+  <p>半径{{ radius }}の円の面積を円周率{{ PI }}で計算すると{{ area }}</p>
 </template>
